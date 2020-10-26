7ff9666a4c2c83aedc82a2cc44eeab706fd80fc8
Ivan Efimov <ivan.efimov@magenta-technology.com>
2020-10-26 18:13:31 +0400

    branch commit

--- src/simple/automation/Main.java
+++ src/simple/automation/Main.java
@@ -3,20 +3,18 @@ package simple.automation;
 import java.util.*;
 
 public class Main {
-    private static final User EXISTED_USER = new User("existed", "");
-    private static final User NOT_EXISTED_USER = new User("wrong", "");
 
     public static void main(String[] args) {
 
         List<String> strings = Arrays.asList("два", "один", "три");
 
         Map<String, Integer> digestMap = new HashMap<>();
-        digestMap.put("один", 1);
+        digestMap.put("branch", 1);
         digestMap.put("два", 2);
         digestMap.put("три", 3);
 
         strings.forEach(str -> System.out.println(digestMap.get(str)));
-
+        String unusedVar;
     }
 
 
