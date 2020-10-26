1d3f5c01a8cdc45506edfe8f811e9bb26321013a
Ivan Efimov <ivan.efimov@magenta-technology.com>
2020-10-26 18:14:23 +0400

    master commit

--- src/simple/automation/Main.java
+++ src/simple/automation/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<String> strings = Arrays.asList("два", "один", "три");
 
         Map<String, Integer> digestMap = new HashMap<>();
-        digestMap.put("один", 1);
+        digestMap.put("master", 1);
         digestMap.put("два", 2);
         digestMap.put("три", 3);
 
