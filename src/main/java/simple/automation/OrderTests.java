a137957597f51803fa7b309bee805eb122cddea0
Ivan Efimov <ivan.efimov@magenta-technology.com>
2020-11-07 17:56:57 +0400

    maven

--- src/main/java/simple/automation/OrderTests.java
+++ src/main/java/simple/automation/OrderTests.java
@@ -1,11 +1,14 @@
 package simple.automation;
 
+import org.junit.Test;
+
 public class OrderTests extends BaseTestWithOrder{
 
     public void testCreateOrder(){
         System.out.println("check that order was created");
     }
 
+    @Test
     public void testListOrders(){
         System.out.println("list orders");
         System.out.println("check that order is displayed");
