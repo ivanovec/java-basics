c50c6fa64fb56edfddbd0eed9e674b8d0c9dac62
Ivan Efimov <ivan.efimov@magenta-technology.com>
2021-01-31 23:49:18 +0400

    junit

--- src/main/java/simple/automation/OrderTests.java
+++ src/main/java/simple/automation/OrderTests.java
@@ -1,17 +1,63 @@
 package simple.automation;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class OrderTests extends BaseTestWithOrder{
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@Smoke
+public class OrderTests {
+
+    @BeforeEach
+    public void createOrder(){
+        System.out.println("create order");
+    }
+
+    @Test
+    @Tag("create")
     public void testCreateOrder(){
         System.out.println("check that order was created");
     }
 
-    @Test
-    public void testListOrders(){
-        System.out.println("list orders");
-        System.out.println("check that order is displayed");
+    @ParameterizedTest
+    @MethodSource
+    public void testListOrdersAsManager(Roles role, String orderType){
+        System.out.println("list orders as " + role.getDescription());
+        System.out.println("check that is displayed orders for " + orderType);
+    }
+
+    static Stream<Arguments> testListOrdersAsManager(){
+        return Stream.of(
+          arguments(Roles.MANAGER, "orders for manager"),
+          arguments(Roles.SENIOR_MANAGER, "orders for senior")
+        );
+    }
+
+    enum Roles{
+        MANAGER("manager"),
+        SENIOR_MANAGER("manager of managers"),
+        LEAD_MANAGER("manager of manager of managers"),
+        HARD_WORKER("just a worker");
+
+        Roles(String description){
+            this.description = description;
+        }
+
+        private String description;
+        public String getDescription(){
+            return description;
+        }
+    }
+
+    @AfterEach
+    public void deleteOrder(){
+        System.out.println("delete order");
     }
 
 }
---
+++ src/main/java/simple/automation/Smoke.java
@@ -0,0 +1,14 @@
+package simple.automation;
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("smoke")
+public @interface Smoke {
+}
