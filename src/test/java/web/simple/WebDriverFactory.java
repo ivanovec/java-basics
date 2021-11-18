f485bdc4054e5fb8a8ad9968bff53fad60b19343
Ivan Efimov <ivan.efimov@magenta-technology.com>
2021-11-19 01:31:28 +0400

    clean

--- src/test/java/web/simple/WebDriverFactory.java
+++ src/test/java/web/simple/WebDriverFactory.java
@@ -61,7 +61,7 @@ public class WebDriverFactory {
 
         ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.addArguments("--headless", "--disable-gpu", "--no-sandbox");
-        chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
 
         return new ChromeDriver(chromeOptions);
     }
