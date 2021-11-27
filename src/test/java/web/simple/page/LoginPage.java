8d2c3fbfcc8b99e2f0a138862e655dc23118aa3d
Ivan Efimov <ivan.efimov@magenta-technology.com>
2021-11-27 11:30:08 +0400

    fix rambler tests

--- src/test/java/web/simple/page/LoginPage.java
+++ src/test/java/web/simple/page/LoginPage.java
@@ -38,7 +38,7 @@ public class LoginPage extends BasePage{
     @Step("Нажатие кнопки логина")
     public SearchPage clickLogin(){
         driver.findElement(processLogin).click();
-        driver.switchTo().window(driver.getWindowHandles().stream().findFirst().get())
+        driver.switchTo().window(driver.getWindowHandles().stream().findFirst().get());
         return new SearchPage(driver);
     }
 
