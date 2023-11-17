c780fb4e276ad94641c47392cb8ce9184c221ff5
Ivan Efimov <ivan.efimov@jetbrains.com>
2023-11-17 13:34:22 +0200

    while true

--- src/main/java/simple/automation/AuthorizationTests.java
+++ src/main/java/simple/automation/AuthorizationTests.java
@@ -9,5 +9,6 @@ public class AuthorizationTests extends BaseTestWithAuthorization{
     @Test
     public void testLogin(){
         System.out.println("check success login");
+        while (true);
     }
 }
