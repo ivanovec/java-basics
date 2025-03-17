5489ddaddb6924c511a8f6a467e9e0fa6337da0b
Ivan Efimov <Ivan.Efimov@jetbrains.com>
2025-03-17 21:00:29 +0200

    test sarif

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -45,13 +45,13 @@ object Build : BuildType({
         script {
             id = "simpleRunner"
             scriptContent = """
-                /bin/cat <<'EOM' >runner.sh
-                            #!/bin/bash
-                            ls -la /data/project
-                            ls -la /data/results
-                            cp /data/project/ui.sarif.json /data/results/qodana.sarif.json 
-                            EOM
-                            chmod +x runner.sh
+                            /bin/cat <<'EOM' >runner.sh
+                                #!/bin/bash
+                                ls -la /data/project
+                                ls -la /data/results
+                                cp /data/project/ui.sarif.json /data/results/qodana.sarif.json 
+                                EOM
+                            chmod 777 runner.sh
             """.trimIndent()
         }
         qodana {
