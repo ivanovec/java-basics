8169c4e078343e29ef72b26c8169fc46fae39642
Ivan Efimov <Ivan.Efimov@jetbrains.com>
2025-04-16 12:42:07 +0300

    123

--- src/main/java/simple/automation/Main.java
+++ src/main/java/simple/automation/Main.java
@@ -1,20 +1,22 @@
 package simple.automation;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        String test = "123";
+
+
+                String test = "123";
         List<String> strings = Arrays.asList("два", "один", "три");
 
-        Map<String, Integer> digestMap = new HashMap<>();
+   Map<String, Integer> digestMap = new HashMap<>();
         digestMap.put("десять", 10);
         digestMap.put("два", 2);
+
         digestMap.put("три", 3);
 
-        strings.forEach(str -> System.out.println(digestMap.get(str)));
+strings.forEach(str -> System.out.println(digestMap.get(str)));
 
     }
 
