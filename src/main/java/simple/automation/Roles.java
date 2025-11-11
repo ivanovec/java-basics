4683ae7a61e163763187cf4df9633e21a7db1542
Ivan <ivan.efimov@jetbrains.com>
2025-11-11 10:56:46 +0200

    Revert "test"
    This reverts commit 9de443a4048d60be7c5f02ba2bd69e2fb7c316fc.

--- src/main/java/simple/automation/Roles.java
+++ src/main/java/simple/automation/Roles.java
@@ -22,8 +22,4 @@ public enum Roles{
     public void anotherStubMethod() {
         // TODO: Implement another stub method
     }
-
-    public void yetAnotherStubMethod() {
-        // TODO: Implement yet another stub method
-    }
 }
