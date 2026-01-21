b4a0ea1ef47f2086a3699f2d9523fd5d0f15d446
Ivan Efimov <ivan.efimov@jetbrains.com>
2026-01-21 18:08:24 +0200

    testd

--- src/main/java/simple/automation/BaseTestWithOrder.java
+++ src/main/java/simple/automation/BaseTestWithOrder.java
@@ -3,5 +3,6 @@ package simple.automation;
 public class BaseTestWithOrder extends BaseTestWithAuthorization{
     public BaseTestWithOrder(){
         System.out.println("create order");
+        String order = "12345";
     }
 }
