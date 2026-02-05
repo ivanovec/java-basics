c47c48c31a5c22b4c32b5ed3f55f13b7ea33faf8
Ivan Efimov <ivan.efimov@jetbrains.com>
2026-02-05 12:18:58 +0200

    cr

--- src/main/java/simple/automation/CircuitBreaker.java
+++ src/main/java/simple/automation/CircuitBreaker.java
@@ -38,6 +38,9 @@ public class CircuitBreaker {
 		String res = "sdffggg";
 
 
+		String x = "1";
+		String y = "2";
+		String z = "3";
 
         this.failureCount = new AtomicInteger(0);
         this.lastFailureTime = new AtomicLong(0);
