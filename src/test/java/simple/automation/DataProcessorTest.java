97a86f1cab9cb4cd40fcc73c3256d0be2996b011
Ivan Efimov <ivan.efimov@jetbrains.com>
2026-02-12 15:57:54 +0200

    cr

---
+++ src/test/java/simple/automation/DataProcessorTest.java
@@ -0,0 +1,21 @@
+package simple.automation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataProcessorTest {
+
+	@Test
+	void testProcessData() {
+		String input = "test";
+		assertNotNull (input);
+		assertEquals (4, input.length ());
+	}
+
+	@Test
+	void testEmptyData() {
+		String empty = "";
+		assertTrue (empty.isEmpty ());
+	}
+}
