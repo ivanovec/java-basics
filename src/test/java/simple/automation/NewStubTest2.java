2c8c94abdedc3acb7593bf1d43d5209a9c92ef38
Ivan Efimov <ivan.efimov@jetbrains.com>
2026-02-17 09:15:09 +0200

    test

---
+++ src/test/java/simple/automation/NewStubTest2.java
@@ -0,0 +1,14 @@
+package simple.automation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewStubTest2 {
+
+    @Test
+    void testStub() {
+        // TODO: implement test
+        fail("Not implemented yet");
+    }
+}
