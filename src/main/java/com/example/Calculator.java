392ac917563b9db4c2517b3072d4d6b65bae0e91
Ivan Efimov <ivan.efimov@jetbrains.com>
2026-07-14 00:07:09 +0300

    test

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return (double) a / b;
+    }
+}
